59ef6585dda59f666401fd067f5d547c6bf4125c
Giovanni Giorgi <giovanni.giorgi@nttdata.com>
2025-07-28 18:03:19 +0200

    Started implementing Gen3

--- src/shellyme/shellyme.py
+++ src/shellyme/shellyme.py
@@ -6,8 +6,12 @@ import ShellyPy
 @click.option('--device-ip')
 def cli(device_ip):
     """Scan Device for status """
+    print(f"Connecting to {device_ip}")
     device=ShellyPy.Shelly(device_ip)
+    print(device.status())
     deviceMeter = device.meter(0)
     power=deviceMeter['power']
     print(f'{device_ip} power={power}') 
 
+if __name__ == '__main__':
+    cli()
